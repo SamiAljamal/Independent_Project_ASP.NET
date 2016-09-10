d037ee4601d62d5c2467816ba3978fa3a7c078fc
Sami Al-Jamal <sami.m.aljamal@gmail.com>
2016-09-09 17:07:58 -0700

    add geojson file.

--- src/IndependentProject/wwwroot/js/map.js
+++ src/IndependentProject/wwwroot/js/map.js
@@ -4,13 +4,19 @@
     function initialize() {
 
         var mapProp = {
-            center: new google.maps.LatLng(20.593684, 78.96288), //India Lat and Lon
+            center: new google.maps.LatLng(0, 0), 
             zoom: 2,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
+            styles: [{ "featureType": "administrative", "elementType": "labels.text.fill", "stylers": [{ "color": "#444444" }] }, { "featureType": "landscape", "elementType": "all", "stylers": [{ "color": "#f2f2f2" }] }, { "featureType": "poi", "elementType": "all", "stylers": [{ "visibility": "off" }] }, { "featureType": "road", "elementType": "all", "stylers": [{ "saturation": -100 }, { "lightness": 45 }] }, { "featureType": "road.highway", "elementType": "all", "stylers": [{ "visibility": "simplified" }] }, { "featureType": "road.arterial", "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, { "featureType": "transit", "elementType": "all", "stylers": [{ "visibility": "off" }] }, { "featureType": "water", "elementType": "all", "stylers": [{ "color": "#46bcec" }, { "visibility": "on" }] }]
         };
         map = new google.maps.Map(document.getElementById("map_canvas"), mapProp);
+
+        var countryLayer = new google.maps.Data();
+        countryLayer.loadGeoJson('https://gist.githubusercontent.com/SamiAljamal/d27ec0e9c17ab88ac92372b31f252251/raw/b398dd324a659b0bed76e02698940eab03dcd762/Countries.json');
+        countryLayer.setMap(map);
     }
 
     google.maps.event.addDomListener(window, 'load', initialize);
 
+
 });
+
