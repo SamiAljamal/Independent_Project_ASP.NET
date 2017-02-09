1a9ca329263f72fc5ea823036571dbe6aad71901
Sami Al-Jamal <sami.m.aljamal@gmail.com>
2017-02-08 23:00:51 -0800

    Prepare files to deploy to heroku.

--- src/IndependentProject/Program.cs
+++ src/IndependentProject/Program.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.CommandLine;
 
 namespace IndependentProject
 {
@@ -11,6 +13,17 @@ namespace IndependentProject
     {
         public static void Main(string[] args)
         {
+            var configurations = new ConfigurationBuilder()
+            .AddCommandLine(args)
+            .Build();
+
+            new WebHostBuilder()
+            .UseConfiguration(configurations) 
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseStartup<Startup>()
+            .Build()
+            .Run();
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
